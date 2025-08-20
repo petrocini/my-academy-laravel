b69dd6d78abf88ca56a4cca440789d06ddd6c107
Petrocini <lucaspetrocinireis@hotmail.com>
2025-08-20 19:24:26 -0300

    remove entrypoint.sh

--- docker/php/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Otimiza a aplicação para produção
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-# Inicia o processo do PHP-FPM em primeiro plano
-exec php-fpm
